6e0eacd9bc59a01ed0cfef5ef04ce9ac4d3bb99a
Blizz991 <martinhblizz@gmail.com>
2021-12-05 13:34:09 +0100

    Attempting to output list of blogposts

---
+++ Orchard Core/ViewModels/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿using OrchardCore.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orchard_Core.ViewModels
+{
+    public class BlogPost : ContentItem
+    {
+        public string Teaser { get; set; }
+    }
+}
